9e9ecd1c012f34f29fcaef981a35b3d06fca52c3
omarzer0 <omarzero1015@gmail.com>
2021-07-11 04:04:44 +0200

    First & add project dependencies

---
+++ app/src/main/java/az/zero/BaseApplication.kt
@@ -0,0 +1,7 @@
+package az.zero
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application()
---
+++ app/src/main/java/az/zero/azshop/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package az.zero.azshop.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import az.zero.azshop.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
